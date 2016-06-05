eee332a8fd1a521343352c6586390322b47e96cc
ritayzy <lslz627@gmail.com>
2016-06-06 01:55:37 +0800

    ignore typings directory

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/gulp-babel/index.d.ts" />
-/// <reference path="globals/gulp/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
